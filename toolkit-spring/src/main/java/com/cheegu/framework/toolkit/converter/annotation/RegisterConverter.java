cb74f8e9be32dad41526c9a715dfe0a245b948fe
taccisum <514162920@qq.com>
2019-01-21 16:29:37 +0800

    deprecate RegisterConverter

--- toolkit-spring/src/main/java/com/cheegu/framework/toolkit/converter/annotation/RegisterConverter.java
+++ toolkit-spring/src/main/java/com/cheegu/framework/toolkit/converter/annotation/RegisterConverter.java
@@ -7,11 +7,13 @@ import java.lang.annotation.*;
 /**
  * @author tac - liaojf@cheegu.com
  * @since 2019/1/21
+ * @deprecated
  */
 @Target(ElementType.TYPE)
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Component
+@Deprecated
 public @interface RegisterConverter {
     boolean register() default true;
 
